9831e5f33766b33f4353e79e115fe50ca30ed2f1
Svetoslav <veiv@mail.ru>
2023-01-20 04:33:06 +0300

    add 20 Jan

--- FirstProject_003/script.js
+++ FirstProject_003/script.js
@@ -37,23 +37,23 @@
 
 // const obj = new Object();
 
-const options = {
-    name: "test",
-    width: 1024,
-    heigth: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log('Test');
-    }
-};
+// const options = {
+//     name: "test",
+//     width: 1024,
+//     heigth: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('Test');
+//     }
+// };
 
-options.makeTest();
+// options.makeTest();
 
-const {border, bg} = options.colors;
-console.log(border);
+// const {border, bg} = options.colors;
+// console.log(border);
 
 // // console.log(options["colors"]["border"]);
 
@@ -76,3 +76,157 @@ console.log(border);
 // console.log(counter);
 
 // console.log(Object.keys(options).length);
+
+
+// МАССИВЫ
+
+
+// let arr = [2, 4, 7 , 1, 5, 0]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(i);
+// }
+
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+// console.log(arr.length);
+
+// ==============================
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// })
+
+//    Разбивание строки в массив и обратно и сортировка
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products);
+// products.sort();
+// console.log(products.join('; '));
+
+// ==============================
+
+//  Сортировка массива с числами:
+
+// const arr1 = [2, 13, 28, 8 , 10];
+// arr1.sort(compareNum);
+// console.log(arr1);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// ==============================
+
+// ПЕРЕДАЧА ПО ССЫЛКЕ ИЛИ ПО ЗНАЧЕНИЮ
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+// // --------
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj;
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+// // --------
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 22;
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     i: 20
+// };
+
+// console.log(Object.assign(numbers, add));
+
+// const clone = Object.assign({}, add);
+// clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+// // ---------
+
+// const oldArray = ['a','b','c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'asdasda'
+
+// console.log(newArray);
+// console.log(oldArray);
+
+// // ----
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'lifejournal', 'bloger'],
+//     internet = [...video,...blogs, 'vk', 'facebook'];
+
+// console.log(internet);
+
+
+// ----
+
+// function log1(a, b ,c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// };
+
+// const numb = [2, 5, 8];
+
+// log1(...numb);
+
+// ----
+
+// const arr2 = ["a", "b"];
+
+// const  newAarray = [...arr2];
+// newAarray[1] = "y";
+// console.log(newAarray);
+// console.log(arr2);
+
+// ----
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newQ = {...q}
+// console.log(newQ);
