f55b099abd15b0bf9373d0b12a5125e95cb4a054
Svetoslav <veiv@mail.ru>
2023-01-24 04:45:55 +0300

    add 24 Jan

---
+++ modul_01_continue/preview/js/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('button');
+
+// btn.onclick = function() {  // Такой код практически не используется
+//     alert('Click');
+// }
+
+btn.addEventListener('click', () => {
+    
+});
