4c99cd901c2329ad43f651352c6408e80d3ed85e
Svetoslav <veiv@mail.ru>
2023-02-12 04:34:39 +0300

    add 12 Feb

---
+++ Practice/calc/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request =  new XMLHttpRequest();
+
+    request.open('GET',);
+});
