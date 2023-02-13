1a90a05c91839e68e77c419bd202d5485f96e2cc
Svetoslav <veiv@mail.ru>
2023-02-13 03:54:41 +0300

    add 13 Feb

--- Practice/calc/js/script.js
+++ Practice/calc/js/script.js
@@ -6,5 +6,25 @@ const inputRub = document.querySelector('#rub'),
 inputRub.addEventListener('input', () => {
     const request =  new XMLHttpRequest();
 
-    request.open('GET',);
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            // console.log(request.response);
+
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    })
+
+    // status
+    // statusText
+    // Response
+    // readyState
+
+
 });
