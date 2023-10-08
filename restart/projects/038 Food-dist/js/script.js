23c138be7116f514c350bb573103deabd07299f8
Svetoslav <veiv@mail.ru>
2023-10-08 09:00:56 +0300

    8 oct

--- restart/projects/038 Food-dist/js/script.js
+++ restart/projects/038 Food-dist/js/script.js
@@ -91,14 +91,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]')
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.toggle('show')
+        document.body.style.overflow = 'hidden'
+        clearInterval(modalTimerId)
+    }
 
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.toggle('show')
-            document.body.style.overflow = 'hidden'
-        })
+        btn.addEventListener('click', openModal)
     })
+
+    
 
     function closeModal() {
         modal.classList.toggle('show')
@@ -119,6 +124,60 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    // const modalTimerId = setTimeout(openModal, 5000)
     
-    
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll)
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll)
+
+
+    // Используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src
+            this.alt = alt
+            this.title = title
+            this.descr = descr
+            this.parent = document.querySelector(parentSelector)
+            this.price = price
+            this.transfer = 100
+            this.changeTuRub()
+        }
+
+        changeTuRub() {
+            this.price = this.price * this.transfer 
+        }
+
+        render() {
+            const element = document.createElement('div')
+            element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена: </div>
+                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                </div>
+            </div>
+            `
+            this.parent.append(element)
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Fitness"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render()
 })
